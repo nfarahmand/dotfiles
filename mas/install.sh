fdb8462fe83b69c7dc4fbab9d986051cb9cd337c
James Boehmer <james.boehmer@gmail.com>
2019-08-15 11:20:21 -0400

    Removed email prompt from mas

--- mas/install.sh
+++ mas/install.sh
@@ -5,8 +5,8 @@
 which brew &>/dev/null || { echo "Install homebrew first." && exit 1; }
 which mas &>/dev/null || brew install mas;
 
-read -p "App Store Email: " email;
-mas signin "${email}";
+# read -p "App Store Email: " email;
+# mas signin "${email}";
 
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mas install $(cat ${BASEDIR}/packages.txt | awk '{print $1}');
