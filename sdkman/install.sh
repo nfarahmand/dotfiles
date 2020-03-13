1b1216999fd5aa57d996a5367760e916940d2de0
James Boehmer <james.boehmer@gmail.com>
2020-03-13 10:32:54 -0400

    Don't updated rc files when installing sdkman

--- sdkman/install.sh
+++ sdkman/install.sh
@@ -2,5 +2,5 @@
 
 [[ "$(uname -s)" == "Darwin" ]] || { echo "Not OSX.  Bye" && exit 0; }
 
-[[ -e "${HOME}/.sdkman/bin/sdkman-init.sh" ]] &>/dev/null || curl -s "https://get.sdkman.io" | bash
+[[ -e "${HOME}/.sdkman/bin/sdkman-init.sh" ]] &>/dev/null || curl -s "https://get.sdkman.io?rcupdate=false" | bash
 
